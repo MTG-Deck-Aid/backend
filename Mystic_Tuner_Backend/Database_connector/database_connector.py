7b290f19b77bdf96f38ab6619f617c7ff503f2e5
Sachin <144852966+SuperSachinS@users.noreply.github.com>
2025-02-24 12:12:26 -0700

    set up update function

--- Mystic_Tuner_Backend/Database_connector/database_connector.py
+++ Mystic_Tuner_Backend/Database_connector/database_connector.py
@@ -1,5 +1,6 @@
 import psycopg2
 from psycopg2 import sql
+from psycopg2.extras import execute_values
 
 class DatabaseConnector():
     __instance = None
@@ -19,9 +20,8 @@ class DatabaseConnector():
         self.password = "admin"  # Your database password
         self.port = "5433"  # Default PostgreSQL port
 
-    def execute_query(self, query, params = None):
         try:
-            connection = psycopg2.connect(
+            self.connection = psycopg2.connect(
                 host=self.host,
                 dbname=self.dbname,
                 user=self.user,
@@ -30,7 +30,13 @@ class DatabaseConnector():
             )
             print("Connection successful")
 
-            cursor = connection.cursor()
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def execute_query(self, query, params = None):
+        try:
+
+            cursor = self.connection.cursor()
 
             if(params == None):
                 cursor.execute(query)
@@ -41,12 +47,6 @@ class DatabaseConnector():
         except Exception as e:
             print(f"Error: {e}")
             return False
-        finally:
-            # Close the cursor and connection if they were created
-            if connection:
-                cursor.close()
-                connection.close()
-                print("Connection closed")
 
 
     def test_connection(self):
@@ -87,4 +87,34 @@ class DatabaseConnector():
         params = (userid,)
         return self.execute_query(query, params)
 
+    def validate_card(self, cardName):
+        query = "SELECT * FROM public.\"CardNames\" where \"name\" = %s;"
+        params = (cardName,)
+        result = self.execute_query(query, params)
+
+        if len(result) > 0:
+            #Card exists in card repository
+            return True
+        else:
+            return False
     
+    def add_cards_to_deck(self, cards, deckid):
+        """
+        arguments:
+        cards: a list of dictionaries with the format of each dictionary being:
+        cardname: string
+        sideboard: boolean
+        cardtype: string (can be None)
+        count: integer
+        
+        deckid: the deck to insert the cards into
+        """
+        query = "INSERT INTO public.\"Card\" (deckid, cardname, sideboard, cardtype, count)VALUES %s"
+        
+        params = [(deckid, card['cardname'], card['sideboard'], card['cardtype'], card['count']) for card in cards]
+
+        execute_values(self.connection.cursor(), query, params)
+
+        self.connection.commit()
+    def delete_cards_from_deck(self, cards, deckid):
+        pass
--- Mystic_Tuner_Backend/Database_connector/test_database_connection.py
+++ Mystic_Tuner_Backend/Database_connector/test_database_connection.py
@@ -1,5 +1,14 @@
 from database_connector import DatabaseConnector 
 def main():
+
+    cards = [
+    {'cardname': 'Card A', 'sideboard': False, 'cardtype': 'Creature', 'count': 3},
+    {'cardname': 'Card B', 'sideboard': True, 'cardtype': 'Sorcery', 'count': 2},
+    {'cardname': 'Card C', 'sideboard': False, 'cardtype': 'Creature', 'count': 4},
+    {'cardname': 'Card D', 'sideboard': True, 'cardtype': 'Artifact', 'count': 1},
+    {'cardname': 'Card E', 'sideboard': False, 'cardtype': 'Instant', 'count': 5},
+    {'cardname': 'Card F', 'sideboard': True, 'cardtype': None, 'count': 2}
+    ]
     instance = DatabaseConnector()
 
     result = instance.test_connection()
@@ -12,6 +21,8 @@ def main():
     response = instance.get_user_decks(1)
     for item in response:
         print(item)
+    
+    instance.delete_cards_from_deck(cards, 1)
 
 if __name__ == "__main__":
     main()
