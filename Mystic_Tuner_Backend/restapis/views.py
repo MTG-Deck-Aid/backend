ad7bea2c3c695c4bf1d4e8ba2cea4958201344b8
Sachin <144852966+SuperSachinS@users.noreply.github.com>
2025-03-11 10:47:49 -0600

    implemented creating a new deck

--- Mystic_Tuner_Backend/restapis/views.py
+++ Mystic_Tuner_Backend/restapis/views.py
@@ -4,6 +4,8 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ParseError
+from Database_Connector.card_queries import CardQueries
+from Database_Connector.deck_queries import DeckQueries
 import json
 
 from Mystic_Tuner_Backend.scryfall_engine.scryfall_engine import ScryFallEngine
@@ -73,8 +75,55 @@ class GetCommander(APIView):
         return Response({'commander': valid_commander.name}, status=status.HTTP_200_OK)
 
 class CreateNewDeck(APIView):
-    def get(self, request):
-        return Response({"TODO"}, status = status.HTTP_418_IM_A_TEAPOT)
+    def post(self, request):
+        try:
+            deck = unpack_file(request)
+        except ParseError as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        
+        commander = deck['commander']
+        deck_name = deck['deckName']
+        
+        user_id = deck['userId']
+        cards = deck['deckList']
+
+        deck_queries = DeckQueries()
+
+        result = deck_queries.add_deck(user_id, 'commander', deck_name, commander)
+
+        if result == False:
+            return Response({'error' : 'Deck name already exists'}, status = status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+        decks = deck_queries.get_user_decks(user_id)
+
+        deck = None
+
+        for item in decks:
+            if item[2] == deck_name:
+                deck = item
+                break
+
+        print(f"deck: {deck}")
+        if deck == None:
+            return Response({'error' : 'couldn\'t create deck'}, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        formatted_list = []
+        for card in cards:
+            formatted_list.append({'cardname': card['name'], 'sideboard': False, 'cardtype': None, 'count': card['quantity']})
+        
+        print(f"formatted_list: {formatted_list}")
+
+        card_queries = CardQueries()
+
+        results = card_queries.add_cards_to_deck(formatted_list, deck[3])
+
+        if result == False:
+            deck_queries.delete_deck(user_id, deck_name)
+            return Response({'error' : 'Could not populate deck'}, status = status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+
+        return Response({"message": "Successfully built deck", 'deckId' : deck[3]}, status = status.HTTP_200_OK)
 
 class GetSuggestions(APIView):
     def get(self, request):
