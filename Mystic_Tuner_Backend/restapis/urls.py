4c3f9b2c7f4a47ee5cb00600c499ad640b3e96f1
Brendan Smiley <bsmiley.lego@gmail.com>
2025-03-12 00:25:22 -0600

    WIP: Card Suggestion API Route

--- Mystic_Tuner_Backend/restapis/urls.py
+++ Mystic_Tuner_Backend/restapis/urls.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponse
 from django.urls import path
-from .views import AuthenticateLoginTokenAPIView
+from .views import AuthenticateLoginTokenAPIView, SuggestionsAPIView
 
 urlpatterns = [
     path('Authenticate/', AuthenticateLoginTokenAPIView.as_view(), name='Authenticate'),
+    path("health-check/", lambda request: HttpResponse("OK"), name="health-check"),
+    path("", lambda request: HttpResponse("Mystic Tuner Backend API"), name="Mystic Tuner Backend"),
+    path("suggestions/", SuggestionsAPIView.as_view(), name="suggestions"),
 ]
