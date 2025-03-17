e508f10fd90002796c7eb423aecf1047df28b3ab
Sachin <144852966+SuperSachinS@users.noreply.github.com>
2025-03-17 10:21:00 -0600

    started experimenting with selenium testing

---
+++ Mystic_Tuner_Backend/Tests/Test_Frontend/test.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+
+driver = webdriver.Edge()
+
+
+driver.get("http://localhost:3000")
+
+button = driver.find_element(By.CLASS_NAME, "bg-sky-blue")
+
+button.click()
