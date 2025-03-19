5c6d3dab47cb5108565fc13316645ff02fb71125
Brendan Smiley <bsmiley.lego@gmail.com>
2025-03-18 20:16:03 -0600

    Fix: see if this fixes the issue

--- Mystic_Tuner_Backend/restapis/middleware.py
+++ Mystic_Tuner_Backend/restapis/middleware.py
@@ -2,7 +2,7 @@ from django.utils.deprecation import MiddlewareMixin
 
 class ReverseProxyMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+        forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR", "").split(",")[0].strip()
         if forwarded_for:
             real_ip = forwarded_for.split(",")[0].strip()
             request.META["REMOTE_ADDR"] = real_ip
